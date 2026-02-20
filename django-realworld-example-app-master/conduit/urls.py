abc83044d874e7ceaa23039398c01f7aa505d9dc
Pablokill2004 <pablo.siquinajay@galileo.edu>
2026-02-20 17:06:57 -0600

    Fix PEP8 violations (E501, F401-Fix code style violations

--- django-realworld-example-app-master/conduit/urls.py
+++ django-realworld-example-app-master/conduit/urls.py
@@ -20,6 +20,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^api/', include('conduit.apps.articles.urls', namespace='articles')),
-    url(r'^api/', include('conduit.apps.authentication.urls', namespace='authentication')),
+    url(r'^api/', include(
+        'conduit.apps.authentication.urls',
+        namespace='authentication'
+    )),
     url(r'^api/', include('conduit.apps.profiles.urls', namespace='profiles')),
 ]
